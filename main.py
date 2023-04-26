a828b0fc3ba401ecd6d6104eb6d1b72ba78e94e3
Arturo Suárez <manuel.suarez@cimat.mx>
2023-04-25 20:38:03 -0600

    Prepare dataset and sample training images

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import torchvision.utils as vutils
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from config import image_size, batch_size, workers, ngpu
 import logging
 
 logging.basicConfig(filename="log.txt", level=logging.INFO)
@@ -24,3 +25,29 @@ seed = 999
 logging.info(f"Random Seed: {seed}")
 random.seed(seed)
 torch.manual_seed(seed)
+
+# We can use an image folder dataset the way we have it setup.
+# Create the dataset
+dataset = dset.ImageFolder(root=dataroot,
+                           transform=transforms.Compose([
+                               transforms.Resize(image_size),
+                               transforms.CenterCrop(image_size),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                           ]))
+
+# Create the dataloader
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                         shuffle=True, num_workers=workers)
+
+# Decide which device we want to run on
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+
+# Plot some training images
+real_batch = next(iter(dataloader))
+plt.figure(figsize=(8, 8))
+plt.axis("off")
+plt.title("Training images")
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2,
+                                         normalize=True).cpu(), (1, 2, 0)))
+plt.savefig("figure1.png")
